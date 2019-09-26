890b045159ca7e38b94f67a5aece1d7ab2f286ed
Prachi ranjan <prachi0198@gmail.com>
2019-09-26 11:02:59 -0700

    centered elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ export class Home extends Component {
         return (
             <div>
                 <h1>speech to sign conveter</h1>
-                <div className="search_box">
+                <center><div className="search_box">
                 <input></input>
                 <button><i class="fal fa-search"></i>Search</button>
-                </div>
+                </div></center>
             </div>
         )
     }
